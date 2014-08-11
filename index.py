d8f4f5754b8f0d27f6cd098cfea29cf212aaef29
Reuben Castelino <projectdelphai@gmail.com>
2014-08-10 22:10:14 -0700

    Initial commit

---
+++ index.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:test@localhost/mangarecs'
+db = SQLAlchemy(app)
+
+class Manga(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    recommender = db.Column(db.Text)
+
+    def __init__(self, name, recommender):
+        self.name = name
+        self.recommender = recommender
+
+    def __repr__(self):
+        return '<Manga: %r>' % self.name
+
+
+@app.route('/')
+def index():
+    return 'Hello /r/manga'
+
+@app.route('/recs/<manga_name>')
+def recommendations(manga_name):
+    manga_name = manga_name.replace('_', ' ')
+    users = Manga.query.filter_by(name=manga_name).all()
+    users = [user.recommender for user in users]
+    manga = Manga.query.filter(Manga.recommender.in_(users)).filter(Manga.name!=manga_name).all()
+    recs = [item.name for item in manga]
+    return render_template('recommendations.html', manga_name=manga_name, recs=recs)
+
+if __name__ == '__main__':
+    app.run(debug=True);
